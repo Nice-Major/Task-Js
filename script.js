f0707d1e0b7da3b730109a7b76e48b6f099190f4
Nice-Major <Aniekwevictor9@gmail.com>
2020-03-31 18:51:47 +0100

    JavaScript script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+let name = "Nice Major";
+
+let courseData = ["HTML", "CSS", "JavaScript"];
+
+for (let i=0; i<=200; i++) {
+  
+  if (courseData.length % 2===i % 2) {
+    
+  console.log(`My name is: ${name}`)
+
+  console.log(`My courses are: ${courseData}`)
+
+  console.log(i);
+    
+  }
+  
+}
